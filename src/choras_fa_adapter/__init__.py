fc4faa48c7376c19897cecce2360099c0ddca226
jan smits <jan@possumstudios.com>
2026-04-28 16:55:26 +0000

    feat: add version attribute to package

--- src/choras_fa_adapter/__init__.py
+++ src/choras_fa_adapter/__init__.py
@@ -4,5 +4,6 @@ from .models import AdapterOutcome
 from .orchestrator import run_from_choras_json
 
 __version__ = "0.1.1"
+version = __version__
 
-__all__ = ["AdapterOutcome", "run_from_choras_json"]
+__all__ = ["AdapterOutcome", "run_from_choras_json", "version"]
